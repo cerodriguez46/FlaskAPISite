e1f49af979786ef1ef6943136aae1d6477c8b407
Christopher-Eri <23482319-Christopher-Eri@users.noreply.replit.com>
2023-05-29 22:03:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs 1,00,000'
+  }, 
+  {
+    'id':2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs 15,00,000'
+  },
+  {
+  'id':3,
+  'title': 'Frontend Engineer',
+  'location': 'Remote',
+  'salary': 'Rs 12,00,000'
+  },
+  {
+  'id':4,
+  'title': 'Backend Engineer',
+  'location': 'San Francisco, USA',
+  'salary': '$120,000'
+  },
+]
+# when a certain url is requested, what should be returned
+@app.route("/")
+def hello_world():
+  return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == "__main__":
+  print("I'm inside the if now")
+  app.run(host='0.0.0.0', debug=True)
+
+
